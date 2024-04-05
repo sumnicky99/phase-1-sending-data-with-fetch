1b0df0a3a41606d5db0a76c79f5fbda65f31e52d
Sumnicky99 <sumnicky99@gmail.com>
2024-04-05 08:19:40 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name, email) {
+    const userObj = {
+        name: name,
+        email: email,
+    }
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userObj)
+    })
+    .then(res => res.json())
+    .then(object => document.body.append(object.id))
+    .catch(function (message) {
+        document.body.append(message)
+    })
+}
